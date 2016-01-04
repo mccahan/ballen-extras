977eb547a41af07934989820456529e965357cf2
Ryan McCahan <ryan@mccahan.net>
2016-01-04 13:26:33 -0700

    Initial Import

---
+++ ballen-extras.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name: Ballen Extras
+ * Description: This plugin provides some extra functionality for Ballen sites
+ * Author: Ryan McCahan
+ */
+
+// Callback function to insert 'styleselect' into the $buttons array
+function ballen_mce_buttons_2( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter('mce_buttons_2', 'ballen_mce_buttons_2');
+
+// Callback function to filter the MCE settings
+function ballen_mce_before_init_insert_formats( $init_array ) {  
+    // Define the style_formats array
+    $style_formats = array(  
+        // Each array child is a format with its own settings
+        array(  
+            'title' => 'Button Link',  
+            'selector' => 'a',  
+            'classes' => 'button'             
+        )
+    );  
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );  
+
+    return $init_array;  
+
+} 
+add_filter( 'tiny_mce_before_init', 'ballen_mce_before_init_insert_formats' );
