e93ac990692bc61cec1cc3880c17f729ffdad062
Ryan McCahan <ryan@mccahan.net>
2016-02-08 22:57:49 -0700

    Kick version number

--- ballen-extras.php
+++ ballen-extras.php
@@ -4,7 +4,7 @@
  * Description: This plugin provides some extra functionality for Ballen sites
  * Author: Ryan McCahan
  * GitHub Plugin URI: https://github.com/mccahan/ballen-extras
- * Version: 1.0.4
+ * Version: 1.0.5
  */
 
 // Callback function to insert 'styleselect' into the $buttons array
